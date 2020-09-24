769a2ec730008de4839d2f04de1d1f96dfffeb21
sawyer-c <61956946+sawyer-c@users.noreply.github.com>
2020-09-23 21:03:49 -0500

    Testing with::

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery with :exception
+	protect_from_forgery with: :exception
 	before_action :authorized
 	helper_method :current_user
 	helper_method :logged_in?
