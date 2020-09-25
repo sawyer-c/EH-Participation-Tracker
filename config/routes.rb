7b19ea8c8e4f40ccd55a5672fd67ed4129e1dd87
nicholastann <54993961+nicholastann@users.noreply.github.com>
2020-09-24 21:24:28 -0500

    removed routing for books

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,4 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
   resources :students
 
-  get 'book/list'
-  get 'book/show'
-  get 'book/new'
-  post 'book/create'
-  get 'book/delete'
-  get 'book/edit'
-  patch 'book/update'
-
-  get 'students/list'
-  get 'students/show_students'
-
-  root 'book#list'
-
 end
