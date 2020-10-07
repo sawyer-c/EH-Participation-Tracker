857eeeb4cca0601e26fe738dfe4920db2a900fdb
SlamJamPham <phillipphm@tamu.edu>
2020-10-07 15:35:28 -0500

    -Added tests for registering new users/logging in with existing user -Added constraints to new student form  +Made standings into a dropdown

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,10 @@
 class Student < ApplicationRecord
+    validates :firstName, presence: true
+    validates :lastName, presence: true
+    validates :email, presence: true
+    validates :gradYear, presence: true
+    validates :major, presence: true
+    validates :status, presence: true
+
+    STANDING_LIST = ["Good Standing", "Bad Standing"]
 end
