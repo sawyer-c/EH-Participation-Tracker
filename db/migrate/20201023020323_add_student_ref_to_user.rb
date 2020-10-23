c9d6a7be906f22c36428474be18849064bb448d7
nicholastann <54993961+nicholastann@users.noreply.github.com>
2020-10-22 22:49:51 -0500

    reverse refrence student & user

--- db/migrate/20201023020323_add_student_ref_to_user.rb
+++ db/migrate/20201023020323_add_student_ref_to_user.rb
@@ -1,5 +1,5 @@
 class AddStudentRefToUser < ActiveRecord::Migration[5.1]
   def change
-    add_reference :users, :student, foreign_key: true
+    add_reference :students, :user, foreign_key: true
   end
 end
