64548e9a34841b5893e9d2dc7979dc1a30969c03
Dawson Abner <dawsonabner@gmail.com>
2020-11-01 21:15:28 -0600

    Export and Import added for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,21 @@
 class Event < ApplicationRecord
+    require 'csv'
+
+    def self.import(file)
+        CSV.foreach(file.path, headers: true) do |row|
+            Event.create! row.to_hash
+        end
+    end
+
+    def self.to_csv
+        attributes = %w{name, date, description, event_type, location}
+
+        CSV.generate(headesr: true) do |csv|
+            csv << attributes
+            all.each do |event|
+                csv << [event.name, event.date, event.description, event.event_type, event.location]
+            end
+        end
+    end
+
 end
