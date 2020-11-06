c0316dc36ada7cc5ec568eece670bb046033168e
nicholastann <54993961+nicholastann@users.noreply.github.com>
2020-11-06 08:12:43 -0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-test_user = User.create([{ email: 'admin0@gmail.com', password: 'T3X@5Ags', role: "2" }])
+test_user = User.create([{ email: 'admin0@gmail.com', password: 'T3X@5Ags', role: "0" }])
 
 test_user2 = User.create([{email: "admin1@gmail.com", password: "T3X@5Ags", role: "1"}])
