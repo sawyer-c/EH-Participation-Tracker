3f16e58ef58242012b97c680ccc79ff24a02dd0c
sawyer-c <sawyer_cowan818@yahoo.com>
2020-11-08 18:14:17 -0600

    Admin Seed Account

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-test_user = User.create([{ email: 'admin0@gmail.com', password: 'T3X@5Ags', role: "member" }])
-
-test_user2 = User.create([{email: "admin1@gmail.com", password: "T3X@5Ags", role: "admin"}])
+shared_admin = User.create([{email: "admin0@gmail.com", password: "T3X@5Ags", role: "admin"}])
