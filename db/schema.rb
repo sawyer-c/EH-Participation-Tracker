84616cc97002e57ae13097b7d695c41d7ef4d31a
nicholastann <54993961+nicholastann@users.noreply.github.com>
2020-11-17 20:18:23 -0600

    validate uniqueness of email and uin for student

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_105_035_325) do
+ActiveRecord::Schema.define(version: 20201105035325) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -55,5 +54,5 @@ ActiveRecord::Schema.define(version: 20_201_105_035_325) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key 'students', 'users'
+  add_foreign_key "students", "users"
 end
